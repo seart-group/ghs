464989f2be9ebd41844fd67af92df31a92355418
Ozren Dabić <dabico@usi.ch>
2020-03-15 14:22:31 +0100

    The same services now exist for supported languages.

--- src/main/java/com/dabico/gseapp/service/SupportedLanguageService.java
+++ src/main/java/com/dabico/gseapp/service/SupportedLanguageService.java
@@ -1,8 +1,10 @@
 package com.dabico.gseapp.service;
 
 import com.dabico.gseapp.dto.SupportedLanguageDto;
+import com.dabico.gseapp.dto.SupportedLanguageDtoList;
 
 public interface SupportedLanguageService {
+    SupportedLanguageDtoList getAll();
     void createOrUpdate(SupportedLanguageDto sldto);
     void delete(Long id);
 }
--- src/main/java/com/dabico/gseapp/service/SupportedLanguageServiceImpl.java
+++ src/main/java/com/dabico/gseapp/service/SupportedLanguageServiceImpl.java
@@ -1,27 +1,40 @@
 package com.dabico.gseapp.service;
 
+import com.dabico.gseapp.converter.SupportedLanguageConverter;
 import com.dabico.gseapp.dto.SupportedLanguageDto;
+import com.dabico.gseapp.dto.SupportedLanguageDtoList;
 import com.dabico.gseapp.model.SupportedLanguage;
 import com.dabico.gseapp.repository.SupportedLanguageRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @FieldDefaults(level = AccessLevel.PRIVATE)
-@RequiredArgsConstructor
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class SupportedLanguageServiceImpl implements SupportedLanguageService {
     SupportedLanguageRepository supportedLanguageRepository;
+    SupportedLanguageConverter supportedLanguageConverter;
 
     @Override
-    public void createOrUpdate(SupportedLanguageDto sldto){
-        SupportedLanguage sl = SupportedLanguage.builder().build();
-        if (sldto.getId() != null){
-            sl = supportedLanguageRepository.findById(sldto.getId()).get();
+    public SupportedLanguageDtoList getAll(){
+        List<SupportedLanguage> languages = supportedLanguageRepository.findAll();
+        List<SupportedLanguageDto> dtos = supportedLanguageConverter.fromLanguagesToLanguagesDto(languages);
+        return SupportedLanguageDtoList.builder().items(dtos).build();
+    }
+
+    @Override
+    public void createOrUpdate(SupportedLanguageDto dto){
+        SupportedLanguage language = SupportedLanguage.builder().build();
+        if (dto.getId() != null){
+            language = supportedLanguageRepository.findById(dto.getId()).orElse(null);
         }
-        sl.setLanguage(sldto.getLanguage());
-        supportedLanguageRepository.save(sl);
+        language.setLanguage(dto.getLanguage());
+        supportedLanguageRepository.save(language);
     }
 
     @Override
