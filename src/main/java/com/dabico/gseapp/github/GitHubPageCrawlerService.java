1ca9ab786e002e361bc13664852968ac2b75f688
Ozren Dabić <dabico@usi.ch>
2020-03-17 13:35:15 +0100

    Added contributor count extraction through Selenium.
    If a page fails to locate contributors using Jsoup, a chomedriver is launched to extract the data. Currently known issues: 1) Sometimes the ChromeDriver.get(...) method may throw an UnreachableBrowserException. This happens rarely and non-deterministically, but it can happen. The exact cause is not known, but for now, whenever chromedriver fails to launch, contributors are set to -1. A job can be created to re-crawl the contributors for these specific repositories. 2) After ~40 created chromedrivers, a IOException, stating that there are "Too many open files". I have yet to find a fix for this...

--- src/main/java/com/dabico/gseapp/github/GitHubPageCrawlerService.java
+++ src/main/java/com/dabico/gseapp/github/GitHubPageCrawlerService.java
@@ -1,25 +1,36 @@
 package com.dabico.gseapp.github;
 
-import com.dabico.gseapp.util.DateUtils;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.UnreachableBrowserException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Date;
 
 import static java.lang.Long.parseLong;
 import static com.dabico.gseapp.util.DateUtils.fromGitDateString;
 
 @Getter
-@RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class GitHubPageCrawlerService {
     static final Logger logger = LoggerFactory.getLogger(GitHubPageCrawlerService.class);
 
@@ -51,8 +62,12 @@ public class GitHubPageCrawlerService {
         commits  = parseLong(normalizeNumberString(summary_ul.get(0).childNode(1).childNode(1).childNode(3).childNode(0).toString()));
         branches = parseLong(normalizeNumberString(summary_ul.get(0).childNode(3).childNode(1).childNode(3).childNode(0).toString()));
         releases = parseLong(normalizeNumberString(summary_ul.get(0).childNode(7).childNode(1).childNode(3).childNode(0).toString()));
-        //TODO fetching contributors can sometimes be unpredictable as they might not load on time
-        //contributors = parseLong(normalizeNumberString(summary_ul.get(0).childNode(9).childNode(1).childNode(3).childNode(0).toString()));
+        Node contributors_a = summary_ul.get(0).childNode(9).childNode(1);
+        if (contributors_a.childNodeSize() < 4){
+            contributors = getContributorsWithSelenium(repoURL);
+        } else {
+            contributors = parseLong(normalizeNumberString(contributors_a.childNode(3).childNode(0).toString()));
+        }
         Elements pagehead_ul = document.getElementsByClass("pagehead-actions flex-shrink-0 ");
         watchers = parseLong(normalizeNumberString(pagehead_ul.get(0).childNode(3).childNode(3).attr("aria-label").split(" ")[0]));
         stars    = parseLong(normalizeNumberString(pagehead_ul.get(0).childNode(5).childNode(3).attr("aria-label").split(" ")[0]));
@@ -84,6 +99,44 @@ public class GitHubPageCrawlerService {
         String date_time = document.getElementsByTag("relative-time").attr("datetime");
         lastCommit = fromGitDateString(date_time);
         lastCommitSHA = document.getElementsByClass("sha user-select-contain").get(0).childNode(0).toString();
+    }
+
+    private long getContributorsWithSelenium(String repoURL) {
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL url = classLoader.getResource("chromedriver");
+        String driverFile = url.getFile();
+        System.setProperty("webdriver.chrome.driver", driverFile);
+        System.setProperty("webdriver.chrome.silentOutput", "true");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        ChromeDriverService service = new ChromeDriverService.Builder()
+                .usingDriverExecutable(new File(driverFile))
+                .build();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        options.setExperimentalOption("useAutomationExtension", false);
+        options.addArguments("start-maximized");
+        options.addArguments("disable-infobars");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disable-dev-shm-usage");
+        capabilities.setCapability(ChromeOptions.CAPABILITY,options);
+        ChromeDriver driver = new ChromeDriver(service,options);
+
+        try {
+            driver.get(repoURL);
+        } catch (UnreachableBrowserException ube){
+            ube.printStackTrace();
+            driver.close();
+            driver.quit();
+            return -1L; //RETURN -1 TO INDICATE COMPLETE FAILURE
+        }
+
+        WebDriverWait wait = new WebDriverWait(driver,5);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#js-repo-pjax-container > div.container-lg.clearfix.new-discussion-timeline.px-3 > div > div.overall-summary.border-bottom-0.mb-0.rounded-bottom-0 > ul > li:nth-child(5) > a > span")));
+        String contributors_str = driver.findElementByCssSelector("#js-repo-pjax-container > div.container-lg.clearfix.new-discussion-timeline.px-3 > div > div.overall-summary.border-bottom-0.mb-0.rounded-bottom-0 > ul > li:nth-child(5) > a > span").getText();
+        driver.close();
+        driver.quit();
+        return Long.parseLong(normalizeNumberString(contributors_str));
     }
 
     private String normalizeNumberString(String input){ return input.trim().replaceAll(",",""); }
