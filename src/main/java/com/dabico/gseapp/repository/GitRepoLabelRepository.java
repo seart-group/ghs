ed10f8f16b650c3d207cbaf4dd02616b33e73e9d
Ozren Dabić <dabico@usi.ch>
2020-03-19 21:12:34 +0100

    Fixed incorrect JPQL query for accessing Repository Labels and Languages
    Also findRepoLabels and findRepoLanguages now returns a List of Labels and Languages instead of a Set.

--- src/main/java/com/dabico/gseapp/repository/GitRepoLabelRepository.java
+++ src/main/java/com/dabico/gseapp/repository/GitRepoLabelRepository.java
@@ -5,9 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.Set;
+import java.util.List;
 
 public interface GitRepoLabelRepository extends JpaRepository<GitRepoLabel,Long> {
-    @Query("select l from GitRepo r left join GitRepoLabel l where r.id = l.repo.id and l.repo.id = (:id)")
-    Set<GitRepoLabel> findRepoLabels(@Param("id") Long id);
+    @Query("select l from GitRepoLabel l left join GitRepo r on l.repo.id = r.id where l.repo.id = (:id)")
+    List<GitRepoLabel> findRepoLabels(@Param("id") Long id);
 }
