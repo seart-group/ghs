49b278e42ae863eff89709ec971514ca8358a614
Ozren Dabić <dabico@usi.ch>
2020-04-08 01:37:26 +0200

    Added new interval type: LongInterval

---
+++ src/main/java/com/dabico/gseapp/util/interval/LongInterval.java
@@ -0,0 +1,27 @@
+package com.dabico.gseapp.util.interval;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import org.javatuples.Pair;
+
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PROTECTED)
+public class LongInterval extends Interval<Long>{
+
+    public LongInterval(Long start, Long end){ super(start,end); }
+
+    public Pair<LongInterval,LongInterval> splitInterval() {
+        if (start.equals(end)){ return null; }
+        Long median = (start + end)/2;
+        LongInterval firstInterval  = new LongInterval(start,median);
+        LongInterval secondInterval = new LongInterval(median,end);
+        return new Pair<>(firstInterval,secondInterval);
+    }
+
+    public String toString() {
+        return this.start + ".." + this.end;
+    }
+}
