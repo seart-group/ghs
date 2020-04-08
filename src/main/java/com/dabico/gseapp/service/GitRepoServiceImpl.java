65802fec70666454b6c2f651450484423aa011c4
Ozren Dabić <dabico@usi.ch>
2020-04-08 21:59:38 +0200

    Removed phantom imports

--- src/main/java/com/dabico/gseapp/service/GitRepoServiceImpl.java
+++ src/main/java/com/dabico/gseapp/service/GitRepoServiceImpl.java
@@ -19,9 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Date;
 import java.util.HashSet;
