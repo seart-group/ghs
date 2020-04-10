056752ebfb9ee9e1236968e179bd99f08cc40e4c
Ozren Dabić <dabico@usi.ch>
2020-04-10 02:36:32 +0200

    Re-enabled other supported languages

--- src/main/resources/db/migration/V1__languages.sql
+++ src/main/resources/db/migration/V1__languages.sql
@@ -1,7 +1,7 @@
 INSERT INTO supported_language (name,added) VALUES ('Java',current_timestamp);
---INSERT INTO supported_language (name,added) VALUES ('Kotlin',current_timestamp);
---INSERT INTO supported_language (name,added) VALUES ('C',current_timestamp);
---INSERT INTO supported_language (name,added) VALUES ('CPP',current_timestamp);
---INSERT INTO supported_language (name,added) VALUES ('Python',current_timestamp);
---INSERT INTO supported_language (name,added) VALUES ('Javascript',current_timestamp);
---INSERT INTO supported_language (name,added) VALUES ('Typescript',current_timestamp);
+INSERT INTO supported_language (name,added) VALUES ('Kotlin',current_timestamp);
+INSERT INTO supported_language (name,added) VALUES ('C',current_timestamp);
+INSERT INTO supported_language (name,added) VALUES ('CPP',current_timestamp);
+INSERT INTO supported_language (name,added) VALUES ('Python',current_timestamp);
+INSERT INTO supported_language (name,added) VALUES ('Javascript',current_timestamp);
+INSERT INTO supported_language (name,added) VALUES ('Typescript',current_timestamp);
