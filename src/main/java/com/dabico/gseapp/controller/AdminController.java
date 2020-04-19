b08855433e70c0acf4e440724fff9549c4ec8960
Ozren Dabić <dabico@usi.ch>
2020-04-19 19:12:31 +0200

    Added CrossOrigin annotations to controller methods

--- src/main/java/com/dabico/gseapp/controller/AdminController.java
+++ src/main/java/com/dabico/gseapp/controller/AdminController.java
@@ -57,6 +57,7 @@ public class AdminController {
         }
     }
 
+    @CrossOrigin
     @GetMapping("/l")
     public ResponseEntity<?> getLanguages(){
         return ResponseEntity.ok(supportedLanguageService.getAll());
@@ -77,6 +78,7 @@ public class AdminController {
         return ResponseEntity.ok(gitRepoService.getAllLanguageStatistics());
     }
 
+    @CrossOrigin
     @GetMapping("/r/stats")
     public ResponseEntity<?> getRepoStatistics(){
         return ResponseEntity.ok(gitRepoService.getMainLanguageStatistics());
--- src/main/java/com/dabico/gseapp/controller/GitRepoController.java
+++ src/main/java/com/dabico/gseapp/controller/GitRepoController.java
@@ -13,10 +13,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityNotFoundException;
 import java.io.File;
@@ -142,6 +139,7 @@ public class GitRepoController {
         }
     }
 
+    @CrossOrigin
     @GetMapping("/r/labels")
     public ResponseEntity<?> getAllLabels(){
         return ResponseEntity.ok(gitRepoService.getAllLabels());
