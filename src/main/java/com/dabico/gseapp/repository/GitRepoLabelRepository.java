843b695e1ea19d5d0c833918187d92a2cdfa6309
Ozren Dabić <dabico@usi.ch>
2020-05-08 20:05:53 +0200

    Labels are also sorted
    This si to place more common labels on top of suggestions

--- src/main/java/com/dabico/gseapp/repository/GitRepoLabelRepository.java
+++ src/main/java/com/dabico/gseapp/repository/GitRepoLabelRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 public interface GitRepoLabelRepository extends JpaRepository<GitRepoLabel,Long> {
     @Query("select l from GitRepoLabel l left join GitRepo r on l.repo.id = r.id where l.repo.id = (:id)")
     List<GitRepoLabel> findRepoLabels(@Param("id") Long id);
-    @Query("select distinct l.label from GitRepoLabel l where l is not null")
+    @Query("select distinct l.label from GitRepoLabel l where l is not null group by l.label order by count(l.label) desc")
     List<String> findAllLabels();
     void deleteAllByRepo(GitRepo repo);
 }
