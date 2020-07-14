3bf3f2e69a59d63b75c8ab2eec3683768e481b19
Ozren Dabić <dabico@usi.ch>
2020-07-14 23:15:24 +0200

    Interval now has a default Interval.toString implementation for subclasses.
    Only DateInterval has a custom implementation that overrides the superclass
    implementation.

--- src/main/java/com/dabico/gseapp/util/interval/DateInterval.java
+++ src/main/java/com/dabico/gseapp/util/interval/DateInterval.java
@@ -11,7 +11,6 @@ import java.util.Date;
 @SuperBuilder
 @FieldDefaults(level = AccessLevel.PROTECTED)
 public class DateInterval extends Interval<Date> {
-
     public Pair<DateInterval,DateInterval> splitInterval(){
         if (start.equals(end)){ return null; }
         Date median = new Date((start.getTime() + end.getTime())/2);
@@ -20,6 +19,7 @@ public class DateInterval extends Interval<Date> {
         return new Pair<>(firstInterval,secondInterval);
     }
 
+    @Override
     public String toString(){
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
         return dateFormat.format(start) + ".." + dateFormat.format(end);
--- src/main/java/com/dabico/gseapp/util/interval/LongInterval.java
+++ src/main/java/com/dabico/gseapp/util/interval/LongInterval.java
@@ -8,16 +8,11 @@ import org.javatuples.Pair;
 @SuperBuilder
 @FieldDefaults(level = AccessLevel.PROTECTED)
 public class LongInterval extends Interval<Long>{
-
     public Pair<LongInterval,LongInterval> splitInterval() {
         if (start.equals(end)){ return null; }
         Long median = (start + end)/2;
         LongInterval firstInterval  = LongInterval.builder().start(start).end(median).build();
         LongInterval secondInterval = LongInterval.builder().start(median).end(end).build();
         return new Pair<>(firstInterval,secondInterval);
-    }
-
-    public String toString(){
-        return this.start + ".." + this.end;
     }
 }
