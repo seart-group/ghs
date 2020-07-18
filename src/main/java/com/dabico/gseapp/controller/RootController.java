ba7e5aef39a77adb0e91c7c17ebb32b392b2c9cc
Ozren Dabić <dabico@usi.ch>
2020-07-18 15:45:59 +0200

    Added debug date to RootController GET

--- src/main/java/com/dabico/gseapp/controller/RootController.java
+++ src/main/java/com/dabico/gseapp/controller/RootController.java
@@ -3,8 +3,11 @@ package com.dabico.gseapp.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
+import java.util.Date;
+
 @RestController
 public class RootController {
     @RequestMapping("/")
-    public String root() { return "Spring Boot is running."; }
+    public String root() { return "Spring Boot is running: " + Date.from(Instant.now()); }
 }
