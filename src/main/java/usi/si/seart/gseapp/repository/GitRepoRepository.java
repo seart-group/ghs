c39f9bca2759930fe3c2a287d1d8f2fb764a1c8c
dabico <dabico@usi.ch>
2022-02-03 01:21:45 +0100

    getLanguageStatistics methods now return Tuples
    Return type from List<Object[]> to List<Tuple>

--- src/main/java/usi/si/seart/gseapp/repository/GitRepoRepository.java
+++ src/main/java/usi/si/seart/gseapp/repository/GitRepoRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import usi.si.seart.gseapp.model.GitRepo;
 import usi.si.seart.gseapp.repository.specification.GitRepoSpecification;
 
+import javax.persistence.Tuple;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -16,7 +17,7 @@ public interface GitRepoRepository extends JpaRepository<GitRepo,Long>, JpaSpeci
     Optional<GitRepo> findGitRepoById(Long id);
     Optional<GitRepo> findGitRepoByName(String name);
     @Query("select distinct r.mainLanguage, count(r) from GitRepo r group by r.mainLanguage order by count(r) desc")
-    List<Object[]> getLanguageStatistics();
+    List<Tuple> getLanguageStatistics();
     @Query("select distinct r.license from GitRepo r where r.license is not null group by r.license order by count(r.license) desc")
     List<String> findAllLicenses();
     @Query("SELECT r.name FROM GitRepo r ORDER BY r.crawled ASC")
