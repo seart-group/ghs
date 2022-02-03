a04544dc24bbf3eef2d6df6ff1bd975e442fba95
dabico <dabico@usi.ch>
2022-02-03 01:36:00 +0100

    Renamed FileSystemResourceCustom to SelfDestructingResource

--- src/main/java/usi/si/seart/gseapp/util/SelfDestructingResource.java
+++ src/main/java/usi/si/seart/gseapp/util/SelfDestructingResource.java
@@ -10,22 +10,22 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 
-public class FileSystemResourceCustom extends FileSystemResource {
+public class SelfDestructingResource extends FileSystemResource {
 
-    public FileSystemResourceCustom(File file) {
+    public SelfDestructingResource(File file) {
         super(file);
     }
 
     @NotNull
     @Override
     public InputStream getInputStream() throws IOException {
-        return new FileInputStreamCustom(super.getFile());
+        return new DestructingInputStream(super.getFile());
     }
 
-    private static final class FileInputStreamCustom extends FileInputStream {
+    private static final class DestructingInputStream extends FileInputStream {
         private final File file;
 
-        public FileInputStreamCustom(File file) throws FileNotFoundException {
+        public DestructingInputStream(File file) throws FileNotFoundException {
             super(file);
             this.file = file;
         }
