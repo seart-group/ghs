547f06e2fb58fe59484cf0858321ed01277f0ac8
dabico <dabico@usi.ch>
2022-02-05 23:18:59 +0100

    De-cluttered the annotations of Criteria implementations

--- src/main/java/usi/si/seart/gseapp/repository/criteria/NestedKeyValueCriteria.java
+++ src/main/java/usi/si/seart/gseapp/repository/criteria/NestedKeyValueCriteria.java
@@ -2,21 +2,13 @@ package usi.si.seart.gseapp.repository.criteria;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 import lombok.experimental.FieldDefaults;
 import usi.si.seart.gseapp.repository.operation.BinaryOperation;
 
 @Getter
-@Setter
-@NoArgsConstructor
 @AllArgsConstructor
-@ToString
-@EqualsAndHashCode
-@FieldDefaults(level = AccessLevel.PRIVATE)
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class NestedKeyValueCriteria implements Criteria {
     String outerKey;
     String innerKey;
