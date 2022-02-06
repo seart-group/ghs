511299116ac22064631648143e4335a9f26d4a47
dabico <dabico@usi.ch>
2022-02-06 11:28:44 +0100

    Refactored typeahead.js API call and controller methods
    The response is now just a plain array. I have no idea
    why I felt like I needed to use a single-attribute
    object. Both the controller return payloads, and the
    client-side handlers are simplified now.

--- src/main/fe-src/js/typeahead.js
+++ src/main/fe-src/js/typeahead.js
@@ -2,23 +2,14 @@ function generateOptions(dataset) {
     return { showHintOnFocus: true, source: dataset, items: 15, minLength: 2 }
 }
 
-fetch("http://localhost:8080/api/r/labels").then(response => {
-    return response.json();
-}).then(data => {
-    let labels = data.items;
-    $label.typeahead(generateOptions(labels));
-}).catch(_ => {});
+function fetchTypeaheadData(url, jqElement) {
+    fetch(url).then(response => {
+        return response.json();
+    }).then(data => {
+        jqElement.typeahead(generateOptions(data));
+    }).catch(_ => {});
+}
 
-fetch("http://localhost:8080/api/l").then(response => {
-    return response.json();
-}).then(data => {
-    let languages = data.items;
-    $language.typeahead(generateOptions(languages));
-}).catch(_ => {});
-
-fetch("http://localhost:8080/api/r/licenses").then(response => {
-    return response.json();
-}).then(data => {
-    let licenses = data.items;
-    $license.typeahead(generateOptions(licenses));
-}).catch(_ => {});
+fetchTypeaheadData("http://localhost:8080/api/r/labels", $label)
+fetchTypeaheadData("http://localhost:8080/api/l", $language)
+fetchTypeaheadData("http://localhost:8080/api/r/licenses", $license)
