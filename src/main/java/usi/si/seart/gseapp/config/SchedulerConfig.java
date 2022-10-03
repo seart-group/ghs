ee38ed62f3370b60b470429696ff763eec491455
dabico <dabico@usi.ch>
2022-10-03 11:59:30 +0200

    Added very basic error handler to scheduler

--- src/main/java/usi/si/seart/gseapp/config/SchedulerConfig.java
+++ src/main/java/usi/si/seart/gseapp/config/SchedulerConfig.java
@@ -1,9 +1,13 @@
 package usi.si.seart.gseapp.config;
 
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.util.ErrorHandler;
 
 import java.time.Clock;
 
@@ -29,7 +33,18 @@ public class SchedulerConfig {
         threadPoolTaskScheduler.setClock(Clock.systemUTC());
         threadPoolTaskScheduler.setPoolSize(3);
         threadPoolTaskScheduler.setThreadNamePrefix("GHSThread");
+        threadPoolTaskScheduler.setErrorHandler(new SchedulerErrorHandler());
         threadPoolTaskScheduler.initialize();
         return threadPoolTaskScheduler;
+    }
+
+    private static class SchedulerErrorHandler implements ErrorHandler {
+
+        private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+        @Override
+        public void handleError(@NotNull Throwable t) {
+            log.error("Unhandled exception occurred while performing a scheduled job.", t);
+        }
     }
 }
