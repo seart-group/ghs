a8d8a6d2aaebd2f468f1c428721e0e102be3affd
dabico <dabico@usi.ch>
2022-10-03 09:57:59 +0200

    Moved caching configuration into its own class

--- src/main/java/usi/si/seart/gseapp/config/MainConfig.java
+++ src/main/java/usi/si/seart/gseapp/config/MainConfig.java
@@ -2,10 +2,6 @@ package usi.si.seart.gseapp.config;
 
 import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCache;
-import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
@@ -18,10 +14,7 @@ import usi.si.seart.gseapp.converter.CrawlJobToDtoConverter;
 import usi.si.seart.gseapp.converter.GitRepoToDtoConverter;
 import usi.si.seart.gseapp.converter.SupportedLanguageToDtoConverter;
 
-import java.util.List;
-
 @Configuration
-@EnableCaching
 public class MainConfig {
     @Bean
     public WebMvcConfigurer webConfigurer() {
@@ -68,17 +61,5 @@ public class MainConfig {
         ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
         threadPoolTaskScheduler.setPoolSize(3);
         return threadPoolTaskScheduler;
-    }
-
-    @Bean
-    public CacheManager cacheManager() {
-        SimpleCacheManager cacheManager = new SimpleCacheManager();
-        cacheManager.setCaches(List.of(
-                new ConcurrentMapCache("labels"),
-                new ConcurrentMapCache("languageStatistics"),
-                new ConcurrentMapCache("licenses"),
-                new ConcurrentMapCache("languages")
-        ));
-        return cacheManager;
     }
 }
