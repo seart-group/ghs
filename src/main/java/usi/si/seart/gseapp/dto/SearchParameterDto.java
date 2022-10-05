c0209c8172eecb8af38265b1d05d930bf5f7f26a
dabico <dabico@usi.ch>
2022-10-05 14:05:29 +0200

    MASSIVE REFACTORING: Search and Download
    Both methods are seeing yet another refactoring. The primary goal was to
    improve the overall signatures of all the controller and service methods.
    In this way, we address the bad smells related to long parameter lists, while
    keeping all the functionalities virtually unchaanged.

---
+++ src/main/java/usi/si/seart/gseapp/dto/SearchParameterDto.java
@@ -0,0 +1,98 @@
+package usi.si.seart.gseapp.dto;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import org.springframework.format.annotation.DateTimeFormat;
+import usi.si.seart.gseapp.util.Ranges;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class SearchParameterDto {
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+    
+    String name = "";
+    Boolean nameEquals = false;
+    String language = "";
+    String license = "";
+    String label = "";
+    
+    Long commitsMin;
+    Long commitsMax;
+    Long contributorsMin;
+    Long contributorsMax;
+    Long issuesMin;
+    Long issuesMax;
+    Long pullsMin;
+    Long pullsMax;
+    Long branchesMin;
+    Long branchesMax;
+    Long releasesMin;
+    Long releasesMax;
+    Long starsMin;
+    Long starsMax;
+    Long watchersMin;
+    Long watchersMax;
+    Long forksMin;
+    Long forksMax;
+    
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    Date createdMin;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    Date createdMax;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    Date committedMin;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    Date committedMax;
+    
+    Boolean excludeForks = false;
+    Boolean onlyForks = false;
+    Boolean hasIssues = false;
+    Boolean hasPulls = false;
+    Boolean hasWiki = false;
+    Boolean hasLicense = false;
+
+    public Map<String, Object> toMap(){
+        return mapper.convertValue(this, new TypeReference<>() {});
+    }
+
+    public Map<String, Object> toParameterMap(){
+        Map<String, Object> parameters = new HashMap<>();
+
+        parameters.put("name", name);
+        parameters.put("nameEquals", nameEquals);
+        parameters.put("language", language);
+        parameters.put("license", license);
+        parameters.put("label", label);
+        parameters.put("commits", Ranges.build(commitsMin, commitsMax));
+        parameters.put("contributors", Ranges.build(contributorsMin, contributorsMax));
+        parameters.put("issues", Ranges.build(issuesMin, issuesMax));
+        parameters.put("pulls", Ranges.build(pullsMin, pullsMax));
+        parameters.put("branches", Ranges.build(branchesMin, branchesMax));
+        parameters.put("releases", Ranges.build(releasesMin, releasesMax));
+        parameters.put("stars", Ranges.build(starsMin, starsMax));
+        parameters.put("watchers", Ranges.build(watchersMin, watchersMax));
+        parameters.put("forks", Ranges.build(forksMin, forksMax));
+        parameters.put("created", Ranges.build(createdMin, createdMax));
+        parameters.put("committed", Ranges.build(committedMin, committedMax));
+        parameters.put("excludeForks", excludeForks);
+        parameters.put("onlyForks", onlyForks);
+        parameters.put("hasIssues", hasIssues);
+        parameters.put("hasPulls", hasPulls);
+        parameters.put("hasWiki", hasWiki);
+        parameters.put("hasLicense", hasLicense);
+
+        return parameters;
+    }
+}
