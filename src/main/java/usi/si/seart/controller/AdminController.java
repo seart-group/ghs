7b4ce2c7ef8fb390673ef46eddc986bbd5fb1d70
dabico <dabico@usi.ch>
2022-10-26 14:23:11 +0200

    Cleanup: removed `getCompletedJobs` endpoint and service methods
    Should not have been publicly exposed in the first place

--- src/main/java/usi/si/seart/controller/AdminController.java
+++ src/main/java/usi/si/seart/controller/AdminController.java
@@ -5,30 +5,22 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.convert.ConversionService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import usi.si.seart.dto.CrawlJobDto;
-import usi.si.seart.model.CrawlJob;
 import usi.si.seart.model.SupportedLanguage;
-import usi.si.seart.service.CrawlJobService;
 import usi.si.seart.service.GitRepoService;
 import usi.si.seart.service.SupportedLanguageService;
 
-import java.util.List;
 import java.util.stream.Collectors;
 
-@SuppressWarnings("ConstantConditions")
 @Slf4j
 @RestController
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class AdminController {
 
-    ConversionService conversionService;
     SupportedLanguageService supportedLanguageService;
-    CrawlJobService crawlJobService;
     GitRepoService gitRepoService;
 
     @GetMapping("/l")
@@ -50,12 +42,5 @@ public class AdminController {
     @GetMapping("/r/stats")
     public ResponseEntity<?> getRepoStatistics(){
         return ResponseEntity.ok(gitRepoService.getMainLanguageStatistics());
-    }
-
-    @GetMapping("/j")
-    public ResponseEntity<?> getCompletedJobs(){
-        List<CrawlJob> jobs = crawlJobService.getCompletedJobs();
-        List<CrawlJobDto> dtos = List.of(conversionService.convert(jobs.toArray(new CrawlJob[0]), CrawlJobDto[].class));
-        return ResponseEntity.ok(dtos);
     }
 }
