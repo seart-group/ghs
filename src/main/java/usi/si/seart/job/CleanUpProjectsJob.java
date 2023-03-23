8b0a1ca836caae7548bb7753f12e89c720bda569
dabico <dabico@usi.ch>
2023-03-23 21:00:24 +0100

    The `CleanUpProjectsJob` now falls back to using `curl`

--- src/main/java/usi/si/seart/job/CleanUpProjectsJob.java
+++ src/main/java/usi/si/seart/job/CleanUpProjectsJob.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 import usi.si.seart.projection.GitRepoView;
 import usi.si.seart.repository.GitRepoRepository;
 
+import java.io.IOException;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -26,8 +27,6 @@ import java.util.function.LongFunction;
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class CleanUpProjectsJob {
 
-    // It's safer to keep projects which we fail to check, rather than removing them from DB.
-    // Let's say there is a bug with our implementation, do we prefer to lose repos one by one?
     private static final int TIMEOUT_RETURN_CODE = -3;
 
     GitRepoRepository gitRepoRepository;
@@ -45,9 +44,8 @@ public class CleanUpProjectsJob {
             GitRepoView view = optional.get();
             Long id = view.getId();
             String name = view.getName();
-            String url = String.format("https://github.com/%s", name);
             log.debug("Checking if {} [id: {}] exists...", name, id);
-            boolean exists = checkIfRepoExists(url);
+            boolean exists = checkIfRepoExists(name);
             if (!exists) {
                 log.info("Found repository without remote [{}], deleting...", name);
                 gitRepoRepository.deleteById(id);
@@ -67,37 +65,56 @@ public class CleanUpProjectsJob {
      * but in my tests SSH would trigger prompts which kill the command.
      * Prompts should remain disabled otherwise GitHub asks credentials for private repos.
      */
-    private boolean checkIfRepoExists(String url) {
+    private boolean checkIfRepoExists(String name) {
+        String url = String.format("https://github.com/%s", name);
         try {
-            ProcessBuilder builder = new ProcessBuilder("git", "ls-remote", url, "--exit-code");
-            builder.environment().put("GIT_TERMINAL_PROMPT", "0");
-            Process process = builder.start();
-            String stderr = IOUtils.toString(process.getErrorStream());
-            boolean exited = process.waitFor(60, TimeUnit.SECONDS);
-            int returnCode;
-            if (exited) {
-                returnCode = process.exitValue();
-            } else {
-                long pid = process.pid();
-                log.debug("Process [{}]: Timed out! Attempting to terminate...", pid);
-                while (process.isAlive()) process.destroyForcibly();
-                log.debug("Process [{}]: Terminated!", pid);
-                returnCode = TIMEOUT_RETURN_CODE;
-            }
-            switch (returnCode) {
-                case 0:
-                    return true;
-                case -3:
-                    throw new TimeoutException("Timed out while using ls-remote!");
-                case 130:
-                    throw new InterruptedException("Process terminated with SIGTERM, exit code " + returnCode);
-                default:
-                    log.debug("Command returned with exit code {}, stderr:\n{}", returnCode, stderr);
-                    return false;
-            }
+            return checkWithGit(url) || checkWithCURL(url);
         } catch (Exception ex) {
+            // It's safer to keep projects which we fail to check, rather than removing them from DB.
+            // Let's say there is a bug with our implementation, do we prefer to lose repos one by one?
             log.error("An exception has occurred during cleanup!", ex);
             return true;
+        }
+    }
+
+    private boolean checkWithGit(String url) throws IOException, InterruptedException, TimeoutException {
+        return executeCommand("git", "ls-remote", url, "--exit-code");
+    }
+
+    private boolean checkWithCURL(String url) throws IOException, InterruptedException, TimeoutException {
+        return executeCommand("curl", "-Is", "--fail-with-body", "--show-error", url);
+    }
+
+    private boolean executeCommand(String... command) throws IOException, InterruptedException, TimeoutException {
+        String joined = String.join(" ", command);
+        log.trace("Executing command: {}", joined);
+        ProcessBuilder builder = new ProcessBuilder(command);
+        builder.environment().put("GIT_TERMINAL_PROMPT", "0");
+        Process process = builder.start();
+
+        String stderr = IOUtils.toString(process.getErrorStream());
+        boolean exited = process.waitFor(60, TimeUnit.SECONDS);
+        int returnCode;
+        if (exited) {
+            returnCode = process.exitValue();
+        } else {
+            long pid = process.pid();
+            log.debug("Process [{}]: Timed out! Attempting to terminate...", pid);
+            while (process.isAlive()) process.destroyForcibly();
+            log.debug("Process [{}]: Terminated!", pid);
+            returnCode = TIMEOUT_RETURN_CODE;
+        }
+
+        switch (returnCode) {
+            case 0:
+                return true;
+            case TIMEOUT_RETURN_CODE:
+                throw new TimeoutException("Timed out while executing command: " + joined);
+            case 130:
+                throw new InterruptedException("Process terminated via SIGTERM, exit code 130.");
+            default:
+                log.debug("Command returned with non-zero exit code {}, stderr:\n{}", returnCode, stderr);
+                return false;
         }
     }
 }
