d2dc3825a71ddaba6032d7bcb19b625f45832378
dabico <dabico@usi.ch>
2023-03-24 19:48:19 +0100

    Added `SupportedLanguage` repository method
    Allows for retrieval by `crawled` progress in ascending order. Good for
    giving priority to languages that have been ignored for some time. Yet
    to be integrated into the crawler.

--- src/main/java/usi/si/seart/repository/SupportedLanguageRepository.java
+++ src/main/java/usi/si/seart/repository/SupportedLanguageRepository.java
@@ -1,10 +1,22 @@
 package usi.si.seart.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import usi.si.seart.model.SupportedLanguage;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SupportedLanguageRepository extends JpaRepository<SupportedLanguage, Long> {
+
     Optional<SupportedLanguage> findByName(String name);
+
+    @Query(
+            "select sl " +
+            "from SupportedLanguage as sl " +
+            "inner join CrawlJob as cj " +
+            "on cj.language.id = sl.id " +
+            "order by cj.crawled"
+    )
+    List<SupportedLanguage> findAllOrderByCrawled();
 }
