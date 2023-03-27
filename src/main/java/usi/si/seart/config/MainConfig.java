a1203e24db97a2f21744f65a2195fda30856c934
dabico <dabico@usi.ch>
2023-03-27 13:33:20 +0200

    Creating converters from `JsonObject` to `RateLimit` and `GitCommit`
    Given that the information used to construct these will be received from
    the GitHub API, we should prefer the usage of a conversion service over
    private methods to make the conversion process reusable. The extraction
    logic is based on the one already present in `GitHubApiService` and
    `GitHubCredentialUtil`. The two converters have also been registered.

--- src/main/java/usi/si/seart/config/MainConfig.java
+++ src/main/java/usi/si/seart/config/MainConfig.java
@@ -9,7 +9,9 @@ import org.springframework.web.filter.ForwardedHeaderFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import usi.si.seart.converter.GitRepoToDtoConverter;
+import usi.si.seart.converter.JsonObjectToGitCommitConverter;
 import usi.si.seart.converter.JsonObjectToGitRepoConverter;
+import usi.si.seart.converter.JsonObjectToRateLimitConverter;
 import usi.si.seart.converter.SupportedLanguageToDtoConverter;
 
 import java.text.DateFormat;
@@ -43,6 +45,8 @@ public class MainConfig {
                  registry.addConverter(new SupportedLanguageToDtoConverter());
                  registry.addConverter(new GitRepoToDtoConverter());
                  registry.addConverter(new JsonObjectToGitRepoConverter());
+                 registry.addConverter(new JsonObjectToGitCommitConverter());
+                 registry.addConverter(new JsonObjectToRateLimitConverter());
             }
         };
     }
