dfaeca9439400b62754fdfcf4aab90656b62f8d5
dabico <dabico@usi.ch>
2023-03-30 16:01:28 +0200

    Added `SupportedLanguage` query: `findAllByNameInOrderByCrawled`
    This retrieves all the `SupportedLanguage` entities whose language name
    is contained within the collection of languages specified as parameter.
    Returned results are sorted by crawl date.

--- src/main/java/usi/si/seart/repository/SupportedLanguageRepository.java
+++ src/main/java/usi/si/seart/repository/SupportedLanguageRepository.java
@@ -2,14 +2,26 @@ package usi.si.seart.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import usi.si.seart.model.SupportedLanguage;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 public interface SupportedLanguageRepository extends JpaRepository<SupportedLanguage, Long> {
 
     Optional<SupportedLanguage> findByName(String name);
+
+    @Query(
+            "select sl " +
+            "from SupportedLanguage as sl " +
+            "inner join CrawlJob as cj " +
+            "on cj.language.id = sl.id " +
+            "where sl.name in (:names) " +
+            "order by cj.crawled"
+    )
+    List<SupportedLanguage> findAllByNameInOrderByCrawled(@Param("names") Collection<String> names);
 
     @Query(
             "select sl " +
