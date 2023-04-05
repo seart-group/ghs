485d21c4af9c5a79669b925ed01f165c9f57fcb8
dabico <dabico@usi.ch>
2023-04-05 11:44:48 +0200

    Updated `HeaderAttachmentInterceptor` to allow multiple headers
    This also marks the inclusion of the API version header for the
    requests. We need this header to mitigate the impact of breaking API
    changes on our crawler.

--- src/main/java/usi/si/seart/http/HeaderAttachmentInterceptor.java
+++ src/main/java/usi/si/seart/http/HeaderAttachmentInterceptor.java
@@ -3,6 +3,7 @@ package usi.si.seart.http;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import okhttp3.Headers;
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -14,14 +15,13 @@ import java.io.IOException;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public final class HeaderAttachmentInterceptor implements Interceptor {
 
-    String name;
-    String value;
+    Headers headers;
 
     @NotNull
     @Override
     public Response intercept(@NotNull Chain chain) throws IOException {
         Request.Builder builder = chain.request().newBuilder();
-        Request request = builder.header(name, value).build();
+        Request request = builder.headers(headers).build();
         return chain.proceed(request);
     }
 }
