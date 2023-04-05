262d6bab843efd24a205d6545f564272191f621e
dabico <dabico@usi.ch>
2023-04-05 23:21:06 +0200

    API root endpoint now prints additional build information

--- src/main/java/usi/si/seart/controller/RootController.java
+++ src/main/java/usi/si/seart/controller/RootController.java
@@ -1,7 +1,10 @@
 package usi.si.seart.controller;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,13 +12,19 @@ import java.time.Instant;
 
 @RestController
 @AllArgsConstructor(onConstructor_ = @Autowired)
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class RootController {
 
-    private final String banner;
+    String banner;
+
+    BuildProperties buildProperties;
 
     @RequestMapping("/")
     public String root() {
+        String name = buildProperties.getName();
+        String version = buildProperties.getVersion();
+        Instant instant = buildProperties.getTime();
         return "<pre style=\"word-wrap: break-word; white-space: pre-wrap;\">"+banner+"</pre>" +
-                "<p>Spring Boot server is running: "+Instant.now()+"</p>";
+                "<p>"+name+", version: "+version+", built on:"+instant+"</p>";
     }
 }
