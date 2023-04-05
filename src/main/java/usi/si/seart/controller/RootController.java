d173bee0e98340ec118c62fc9cba8bec79bc0dcf
dabico <dabico@usi.ch>
2023-04-05 22:55:43 +0200

    Improved the root controller mapping
    Taking inspiration from https://api.github.com/octocat, we also print
    our own group banner.

--- src/main/java/usi/si/seart/controller/RootController.java
+++ src/main/java/usi/si/seart/controller/RootController.java
@@ -1,13 +1,21 @@
 package usi.si.seart.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
-import java.util.Date;
 
 @RestController
+@AllArgsConstructor(onConstructor_ = @Autowired)
 public class RootController {
+
+    private final String banner;
+
     @RequestMapping("/")
-    public String root() { return "Spring Boot is running: " + Date.from(Instant.now()); }
+    public String root() {
+        return "<pre style=\"word-wrap: break-word; white-space: pre-wrap;\">"+banner+"</pre>" +
+                "<p>Spring Boot server is running: "+Instant.now()+"</p>";
+    }
 }
