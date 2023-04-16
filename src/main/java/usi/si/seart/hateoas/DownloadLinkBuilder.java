e476b3d730cd11773675539ad4175a215640c65f
dabico <dabico@usi.ch>
2023-04-16 21:00:05 +0200

    Added `LinkBuilder` hierarchy
    These are components aimed at isolating the link construction behaviour
    for the search. Subsequent commits will replace the old code in favor of
    these new components.

---
+++ src/main/java/usi/si/seart/hateoas/DownloadLinkBuilder.java
@@ -0,0 +1,63 @@
+package usi.si.seart.hateoas;
+
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.UriTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.UriComponentsBuilder;
+import usi.si.seart.controller.GitRepoController;
+import usi.si.seart.dto.SearchParameterDto;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+import java.net.URI;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component("downloadLinkBuilder")
+@AllArgsConstructor(onConstructor_ = @Autowired)
+public class DownloadLinkBuilder extends LinkBuilder<Void> {
+
+    @Qualifier("exportFormats")
+    private final Set<String> exportFormats;
+
+    @Override
+    protected Class<?> getControllerClass() {
+        return GitRepoController.class;
+    }
+
+    @Override
+    @SneakyThrows(NoSuchMethodException.class)
+    protected Method getControllerMethod() {
+        return getControllerClass().getMethod(
+                "downloadRepos",
+                String.class,
+                SearchParameterDto.class,
+                HttpServletResponse.class
+        );
+    }
+
+    @Override
+    public String getLinks(HttpServletRequest request, Void ignored) {
+        MultiValueMap<String, String> parameters = extractParameters(request);
+        UriTemplate template = getUriTemplate();
+        return exportFormats.stream()
+                .map(format -> {
+                    URI base = template.expand(format);
+                    URI uri = UriComponentsBuilder.fromUri(base)
+                            .queryParams(parameters)
+                            .replaceQueryParam("page")
+                            .replaceQueryParam("sort")
+                            .build(true)
+                            .toUri();
+                    Link link = Link.of(uri.toString(), format);
+                    return link.toString();
+                })
+                .collect(Collectors.joining(","));
+    }
+}
---
+++ src/main/java/usi/si/seart/hateoas/SearchLinkBuilder.java
@@ -0,0 +1,123 @@
+package usi.si.seart.hateoas;
+
+import lombok.SneakyThrows;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.UriTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.UriComponentsBuilder;
+import usi.si.seart.controller.GitRepoController;
+import usi.si.seart.dto.SearchParameterDto;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component("searchLinkBuilder")
+public class SearchLinkBuilder extends LinkBuilder<Page<?>> {
+
+    @Override
+    protected Class<?> getControllerClass() {
+        return GitRepoController.class;
+    }
+
+    @Override
+    @SneakyThrows(NoSuchMethodException.class)
+    protected Method getControllerMethod() {
+        return getControllerClass().getMethod(
+                "searchRepos",
+                SearchParameterDto.class,
+                Pageable.class,
+                HttpServletRequest.class
+        );
+    }
+
+    @Override
+    public String getLinks(HttpServletRequest request, Page<?> page) {
+        MultiValueMap<String, String> parameters = extractParameters(request);
+        UriTemplate template = getUriTemplate();
+        URI base = template.expand();
+        Pageable pageable = page.getPageable();
+        int current = pageable.getPageNumber();
+        int total = page.getTotalPages();
+
+        List<Link> links = new ArrayList<>();
+
+        links.add(
+                Link.of(
+                        UriComponentsBuilder.fromUri(base)
+                                .queryParams(parameters)
+                                .build(true)
+                                .toUri()
+                                .toString(),
+                        IanaLinkRelations.SELF
+                )
+        );
+
+        if (!page.isFirst()) {
+            links.add(
+                    Link.of(
+                            UriComponentsBuilder.fromUri(base)
+                                    .queryParams(parameters)
+                                    .replaceQueryParam("page", 0)
+                                    .build(true)
+                                    .toUri()
+                                    .toString(),
+                            IanaLinkRelations.FIRST
+                    )
+            );
+        }
+
+        if (page.hasPrevious()) {
+            links.add(
+                    Link.of(
+                            UriComponentsBuilder.fromUri(base)
+                                    .queryParams(parameters)
+                                    .replaceQueryParam("page", current - 1)
+                                    .build(true)
+                                    .toUri()
+                                    .toString(),
+                            IanaLinkRelations.PREV
+                    )
+            );
+        }
+
+        if (page.hasNext()) {
+            links.add(
+                    Link.of(
+                            UriComponentsBuilder.fromUri(base)
+                                    .queryParams(parameters)
+                                    .replaceQueryParam("page", current + 1)
+                                    .build(true)
+                                    .toUri()
+                                    .toString(),
+                            IanaLinkRelations.NEXT
+                    )
+            );
+        }
+
+        if (!page.isLast()) {
+            links.add(
+                    Link.of(
+                            UriComponentsBuilder.fromUri(base)
+                                    .queryParams(parameters)
+                                    .replaceQueryParam("page", total - 1)
+                                    .build(true)
+                                    .toUri()
+                                    .toString(),
+                            IanaLinkRelations.LAST
+                    )
+            );
+        }
+
+        return links.stream()
+                .map(Link::toString)
+                .collect(Collectors.joining(","));
+    }
+}
