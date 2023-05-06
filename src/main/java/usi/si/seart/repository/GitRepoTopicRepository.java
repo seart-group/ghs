baeabe873a097a1e621e194f6fa3279f9984c75d
Albert Cerfeda <cerfeda.albert@gmail.com>
2023-05-06 18:13:04 +0200

    Added mining of repository topics

--- src/main/java/usi/si/seart/repository/GitRepoTopicRepository.java
+++ src/main/java/usi/si/seart/repository/GitRepoTopicRepository.java
@@ -4,11 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import usi.si.seart.model.GitRepo;
 import usi.si.seart.model.GitRepoTopic;
 import usi.si.seart.model.GitRepoTopicKey;
-import usi.si.seart.model.Topic;
-
-import java.util.Optional;
 
 public interface GitRepoTopicRepository extends JpaRepository<GitRepoTopic, GitRepoTopicKey> {
 
-    Optional<GitRepoTopic> findByTopicAndRepo(Topic topic, GitRepo repo);
+    void deleteAllByRepo(GitRepo repo);
 }
