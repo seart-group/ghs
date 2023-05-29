1c917e8e67e1e72a30f08aa93b074daf3439b26b
dabico <dabico@usi.ch>
2023-05-29 09:56:13 +0200

    Updated `repository` package for new changes

--- src/main/java/usi/si/seart/repository/GitRepoLanguageRepository.java
+++ src/main/java/usi/si/seart/repository/GitRepoLanguageRepository.java
@@ -10,17 +10,10 @@ import java.util.List;
 public interface GitRepoLanguageRepository extends JpaRepository<GitRepoLanguage, Long> {
 
     @Query(
-            "select distinct l.language " +
-            "from GitRepoLanguage l " +
-            "where l is not null"
-    )
-    List<String> findAllLanguages();
-
-    @Query(
-            "select distinct l.language, sum(l.sizeOfCode) " +
+            "select distinct l.language, SUM(l.sizeOfCode) as total " +
             "from GitRepoLanguage l " +
             "group by l.language " +
-            "order by sum(l.sizeOfCode) desc"
+            "order by total desc"
     )
     List<Tuple> getLanguageStatistics();
 }
