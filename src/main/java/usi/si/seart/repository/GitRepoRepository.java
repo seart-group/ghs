7cfa08a7192e4083b827b792f3893862c170a58d
dabico <dabico@usi.ch>
2023-05-30 10:06:21 +0200

    Removing unused `service` and `repository` APIs

--- src/main/java/usi/si/seart/repository/GitRepoRepository.java
+++ src/main/java/usi/si/seart/repository/GitRepoRepository.java
@@ -1,6 +1,5 @@
 package usi.si.seart.repository;
 
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,8 +10,6 @@ import usi.si.seart.repository.specification.GitRepoSearch;
 import usi.si.seart.repository.specification.GitRepoSpecification;
 import usi.si.seart.repository.specification.JpaStreamableSpecificationRepository;
 
-import javax.persistence.Tuple;
-import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 
@@ -25,23 +22,6 @@ public interface GitRepoRepository extends
     Optional<GitRepo> findGitRepoById(Long id);
 
     Optional<GitRepo> findGitRepoByNameIgnoreCase(String name);
-
-    @Query(
-            "select distinct r.mainLanguage, count(r) from GitRepo r " +
-            "group by r.mainLanguage " +
-            "order by count(r) desc"
-    )
-    @Cacheable(value = "languageStatistics")
-    List<Tuple> getLanguageStatistics();
-
-    @Query(
-            "select distinct r.license from GitRepo r " +
-            "where r.license is not null " +
-            "group by r.license " +
-            "order by count(r.license) desc"
-    )
-    @Cacheable(value = "licenses")
-    List<String> findAllLicenses();
 
     @Query(
             "select r.id from GitRepo r " +
