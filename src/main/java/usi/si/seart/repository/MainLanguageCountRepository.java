14d77f18ca92f04b639a1c9ab2c4d0db8c8cbdc7
dabico <dabico@usi.ch>
2023-05-30 09:38:51 +0200

    Added dedicated read-only repositories for new `model.view` classes

---
+++ src/main/java/usi/si/seart/repository/MainLanguageCountRepository.java
@@ -0,0 +1,6 @@
+package usi.si.seart.repository;
+
+import usi.si.seart.model.view.MainLanguageCount;
+
+public interface MainLanguageCountRepository extends ReadOnlyRepository<MainLanguageCount, String> {
+}
