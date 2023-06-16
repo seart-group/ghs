a18263347bc1a4ad5ead24a901232cddd3c61904
dabico <dabico@usi.ch>
2023-06-16 11:08:36 +0200

    Updating security config to remove deprecated API usage

--- src/main/java/usi/si/seart/config/SecurityConfig.java
+++ src/main/java/usi/si/seart/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -15,10 +16,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .csrf().disable()
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeRequests().antMatchers("/actuator/**").authenticated().anyRequest().permitAll()
-                .and().httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(registry -> {
+                    registry.requestMatchers("/actuator/**")
+                            .authenticated()
+                            .anyRequest()
+                            .permitAll();
+                })
+                .httpBasic(Customizer.withDefaults())
                 .build();
     }
 }
