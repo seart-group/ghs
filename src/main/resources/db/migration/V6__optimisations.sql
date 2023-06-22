cca559e5bf2c0d462cb11db62b1f9bad88cc621d
dabico <dabico@usi.ch>
2023-06-22 14:23:06 +0200

    We now make a distinction between `lines` and `lines_non_blank`
    Although we will probably only use the latter for the querying.
    The former might be useful to us at some other point, or maybe
    even other internal stuff.

--- src/main/resources/db/migration/V6__optimisations.sql
+++ src/main/resources/db/migration/V6__optimisations.sql
@@ -3,14 +3,18 @@ ALTER TABLE repo_metrics MODIFY `lines_code` BIGINT NOT NULL DEFAULT 0;
 ALTER TABLE repo_metrics MODIFY `lines_comment` BIGINT NOT NULL DEFAULT 0;
 
 ALTER TABLE repo_metrics ADD COLUMN `lines` BIGINT
-AS (`lines_blank` + `lines_code` + `lines_comment`) VIRTUAL NOT NULL;
+GENERATED ALWAYS AS (`lines_blank` + `lines_code` + `lines_comment`) STORED NOT NULL;
+
+ALTER TABLE repo_metrics ADD COLUMN `lines_non_blank` BIGINT
+GENERATED ALWAYS AS (`lines_code` + `lines_comment`) STORED NOT NULL;
 
 CREATE OR REPLACE VIEW repo_metrics_by_repo AS
 SELECT
     `repo_id`,
-    SUM(`lines`) AS `lines`,
     SUM(`lines_blank`) AS `lines_blank`,
     SUM(`lines_code`) AS `lines_code`,
-    SUM(`lines_comment`) AS `lines_comment`
+    SUM(`lines_comment`) AS `lines_comment`,
+    SUM(`lines`) AS `lines`,
+    SUM(`lines_non_blank`) AS `lines_non_blank`
 FROM `repo_metrics`
 GROUP BY `repo_id`;
