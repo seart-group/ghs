c8b7af171681c2fe2e88a3696a178befcc25e079
dabico <dabico@usi.ch>
2023-06-30 11:49:27 +0200

    Adding migration script that renames all the `repo` tables

---
+++ src/main/resources/db/migration/V7__naming.sql
@@ -0,0 +1,5 @@
+RENAME TABLE repo TO git_repo;
+RENAME TABLE repo_label TO git_repo_label;
+RENAME TABLE repo_language TO git_repo_language;
+RENAME TABLE repo_metrics TO git_repo_metrics;
+RENAME TABLE repo_topic TO git_repo_topic;
