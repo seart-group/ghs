26a394011945523fde8ce119cd1b84e11a18b847
dabico <dabico@usi.ch>
2023-07-17 12:42:48 +0200

    Simplified logic of `createOrUpdateRepo`

--- src/main/java/usi/si/seart/service/GitRepoService.java
+++ src/main/java/usi/si/seart/service/GitRepoService.java
@@ -13,7 +13,6 @@ import usi.si.seart.repository.GitRepoRepository;
 import usi.si.seart.repository.specification.GitRepoSearch;
 
 import javax.persistence.EntityNotFoundException;
-import java.util.Optional;
 import java.util.stream.Stream;
 
 public interface GitRepoService {
@@ -47,38 +46,12 @@ public interface GitRepoService {
 
         @Override
         public GitRepo createOrUpdateRepo(GitRepo repo) {
-            Optional<GitRepo> opt = gitRepoRepository.findGitRepoByNameIgnoreCase(repo.getName());
-            if (opt.isPresent()) {
-                GitRepo existing = opt.get();
-                existing.setIsFork(repo.getIsFork());
-                existing.setCommits(repo.getCommits());
-                existing.setBranches(repo.getBranches());
-                existing.setDefaultBranch(repo.getDefaultBranch());
-                existing.setReleases(repo.getReleases());
-                existing.setContributors(repo.getContributors());
-                existing.setLicense(repo.getLicense());
-                existing.setWatchers(repo.getWatchers());
-                existing.setStargazers(repo.getStargazers());
-                existing.setForks(repo.getForks());
-                existing.setSize(repo.getSize());
-                existing.setCreatedAt(repo.getCreatedAt());
-                existing.setPushedAt(repo.getPushedAt());
-                existing.setUpdatedAt(repo.getUpdatedAt());
-                existing.setHomepage(repo.getHomepage());
-                existing.setMainLanguage(repo.getMainLanguage());
-                existing.setOpenIssues(repo.getOpenIssues());
-                existing.setTotalIssues(repo.getTotalIssues());
-                existing.setOpenPullRequests(repo.getOpenPullRequests());
-                existing.setTotalPullRequests(repo.getTotalPullRequests());
-                existing.setLastCommit(repo.getLastCommit());
-                existing.setLastCommitSHA(repo.getLastCommitSHA());
-                existing.setHasWiki(repo.getHasWiki());
-                existing.setIsArchived(repo.getIsArchived());
-                existing.setCrawled(repo.getCrawled());
-                return gitRepoRepository.save(existing);
-            } else {
-                return gitRepoRepository.save(repo);
-            }
+            String name = repo.getName();
+            Long id = gitRepoRepository.findGitRepoByNameIgnoreCase(name)
+                    .map(GitRepo::getId)
+                    .orElse(null);
+            repo.setId(id);
+            return gitRepoRepository.save(repo);
         }
 
         @Override
