9ff26968a00010566703c4fe4298dee15c10adf4
dabico <dabico@usi.ch>
2023-08-04 15:42:09 +0200

    Replacing four converters with a `GenericConverter` implementation

---
+++ src/main/java/usi/si/seart/converter/GenericStringToJsonElementConverter.java
@@ -0,0 +1,43 @@
+package usi.si.seart.converter;
+
+import com.google.common.base.Strings;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import lombok.AllArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.TypeDescriptor;
+import org.springframework.core.convert.converter.GenericConverter;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+@AllArgsConstructor(onConstructor_ = @Autowired)
+public class GenericStringToJsonElementConverter implements GenericConverter {
+
+    private final Gson gson;
+
+    @Override
+    public Set<ConvertiblePair> getConvertibleTypes() {
+        return Set.of(
+                new ConvertiblePair(String.class, JsonElement.class),
+                new ConvertiblePair(String.class, JsonObject.class),
+                new ConvertiblePair(String.class, JsonArray.class),
+                new ConvertiblePair(String.class, JsonPrimitive.class)
+        );
+    }
+
+    @Override
+    public Object convert(Object source, @NotNull TypeDescriptor sourceType, @NotNull TypeDescriptor targetType) {
+        String input = (String) source;
+        String cleaned = Strings.emptyToNull(input.trim());
+        if (cleaned == null)
+            return JsonNull.INSTANCE;
+        return gson.fromJson(cleaned, targetType.getType());
+    }
+}
