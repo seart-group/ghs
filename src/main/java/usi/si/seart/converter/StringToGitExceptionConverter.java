4c3797e8c06770e0f5e9a4e4b0431e24ac434abd
dabico <dabico@usi.ch>
2023-08-04 22:50:00 +0200

    Added special exception case that was observed while analyzing

--- src/main/java/usi/si/seart/converter/StringToGitExceptionConverter.java
+++ src/main/java/usi/si/seart/converter/StringToGitExceptionConverter.java
@@ -5,6 +5,7 @@ import org.springframework.lang.NonNull;
 import usi.si.seart.exception.git.CheckoutException;
 import usi.si.seart.exception.git.CompressionException;
 import usi.si.seart.exception.git.GitException;
+import usi.si.seart.exception.git.RepositoryDisabledException;
 import usi.si.seart.exception.git.RepositoryNotFoundException;
 import usi.si.seart.exception.git.TerminalPromptsDisabledException;
 import usi.si.seart.exception.git.config.InvalidProxyConfigurationException;
@@ -16,6 +17,7 @@ public class StringToGitExceptionConverter implements Converter<String, GitExcep
     private static final String NOT_FOUND = "not found";
     private static final String EARLY_EOF = "early EOF";
     private static final String LONG_FILE_NAME = "Filename too long";
+    private static final String FORBIDDEN = "The requested URL returned error: 403";
     private static final String UNRESOLVABLE_HOST = "Could not resolve host: github.com";
     private static final String CHECKOUT_FAILED = "unable to checkout working tree";
     private static final String AUTHENTICATION_REQUIRED =
@@ -33,6 +35,8 @@ public class StringToGitExceptionConverter implements Converter<String, GitExcep
                 .orElse(EMPTY);
         if (fatal.endsWith(NOT_FOUND))
             return new RepositoryNotFoundException(fatal);
+        if (fatal.endsWith(FORBIDDEN))
+            return new RepositoryDisabledException(fatal);
         if (fatal.endsWith(UNRESOLVABLE_HOST))
             return new InvalidProxyConfigurationException(fatal);
         if (fatal.endsWith(LONG_FILE_NAME))
