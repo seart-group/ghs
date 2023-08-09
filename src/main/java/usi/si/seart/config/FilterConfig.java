320ebe2dc89cf8062aeff8b6b475faa14c70089b
dabico <dabico@usi.ch>
2023-08-09 15:22:54 +0200

    Filtering the `search` and `download` endpoint for repository

--- src/main/java/usi/si/seart/config/FilterConfig.java
+++ src/main/java/usi/si/seart/config/FilterConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ForwardedHeaderFilter;
+import usi.si.seart.web.filter.RateLimitingFilter;
 
 @Configuration
 public class FilterConfig {
@@ -12,6 +13,15 @@ public class FilterConfig {
     public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
         FilterRegistrationBean<ForwardedHeaderFilter> bean = new FilterRegistrationBean<>();
         bean.setFilter(new ForwardedHeaderFilter());
+        return bean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<RateLimitingFilter> rateLimitingFilter() {
+        FilterRegistrationBean<RateLimitingFilter> bean = new FilterRegistrationBean<>();
+        bean.setFilter(new RateLimitingFilter());
+        bean.addUrlPatterns("/r/search", "/r/download/*");
+        bean.setOrder(1);
         return bean;
     }
 }
