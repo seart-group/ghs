913c833dfc904fa47931777dce1f4260c74fb550
dabico <dabico@usi.ch>
2023-08-11 14:14:32 +0200

    Added a jQuery utility for spawning alerts

--- html/js/jquery.js
+++ html/js/jquery.js
@@ -1,6 +1,8 @@
-(function ($, crypto, Handlebars, Toast) {
+(function ($, crypto, Handlebars, Toast, Alert) {
     const $toast_template = $("#template-toast").html();
     const $toast_content = Handlebars.compile($toast_template);
+    const $alert_template = $("#template-alert").html();
+    const $alert_content = Handlebars.compile($alert_template);
 
     // https://stackoverflow.com/a/2117523/17173324
     const UUIDV4 = function () {
@@ -22,4 +24,18 @@
         $element.on("hidden.bs.toast", () => $element.remove());
         return this;
     };
-}(jQuery, crypto, Handlebars, bootstrap.Toast));
+
+    $.fn.twbsAlert = function (options) {
+        const context = $.extend({
+            id: `alert-${UUIDV4()}`,
+            body: "Lorem Ipsum"
+        }, options);
+        const html = $alert_content(context);
+        this.append(html);
+        const $element = this.find(`#${context.id}`);
+        const [ element ] = $element.get();
+        new Alert(element);
+        $element.on("closed.bs.alert", () => $element.remove());
+        return this;
+    };
+}(jQuery, crypto, Handlebars, bootstrap.Toast, bootstrap.Alert));
