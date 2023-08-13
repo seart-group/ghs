30377ef679acc3d96aae8209b1f54d37d0692053
dabico <dabico@usi.ch>
2023-08-13 23:39:40 +0200

    Added support for ternary operators like BETWEEN

--- src/main/java/ch/usi/si/seart/repository/operation/Operation.java
+++ src/main/java/ch/usi/si/seart/repository/operation/Operation.java
@@ -1,6 +1,6 @@
 package ch.usi.si.seart.repository.operation;
 
-public sealed interface Operation permits UnaryOperation, BinaryOperation {
+public sealed interface Operation permits UnaryOperation, BinaryOperation, TernaryOperation {
 
     String name();
 }
---
+++ src/main/java/ch/usi/si/seart/repository/operation/TernaryOperation.java
@@ -0,0 +1,5 @@
+package ch.usi.si.seart.repository.operation;
+
+public enum TernaryOperation implements Operation {
+    BETWEEN
+}
