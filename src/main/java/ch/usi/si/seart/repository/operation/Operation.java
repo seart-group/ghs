9a2ff0eb9edb931353dbdf831238e061ed1af2b0
dabico <dabico@usi.ch>
2023-08-13 19:10:16 +0200

    Removing the now-unused method

--- src/main/java/ch/usi/si/seart/repository/operation/Operation.java
+++ src/main/java/ch/usi/si/seart/repository/operation/Operation.java
@@ -3,10 +3,4 @@ package ch.usi.si.seart.repository.operation;
 public interface Operation {
 
     String name();
-
-    default RuntimeException toRuntimeException() {
-        return new UnsupportedOperationException(
-                "Operation: [" + name() + "] not supported!"
-        );
-    }
 }
