83bf579950f836d4b2e03a25b89848dabd6fb364
dabico <dabico@usi.ch>
2023-11-14 16:21:49 +0100

    Specifying custom `PropertySource` file

--- src/main/java/ch/usi/si/seart/config/PropertiesConfig.java
+++ src/main/java/ch/usi/si/seart/config/PropertiesConfig.java
@@ -3,9 +3,11 @@ package ch.usi.si.seart.config;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @EnableConfigurationProperties
 @ConfigurationPropertiesScan(basePackages = "ch.usi.si.seart.config.properties")
+@PropertySource("classpath:crawler.properties")
 public class PropertiesConfig {
 }
