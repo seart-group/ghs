55466cf10e040a80d55fdb5e5b8765579d5ffb7b
dabico <dabico@usi.ch>
2023-11-15 12:15:29 +0100

    Returning configuration to `application.properties`
    Unfortunately, using the separate file breaks many of the SPeL
    conditions that we check in order to spawn various scheduled tasks and
    the like. I may revert this change if I find a more suitable method of
    checking configuration properties

--- src/main/java/ch/usi/si/seart/config/PropertiesConfig.java
+++ src/main/java/ch/usi/si/seart/config/PropertiesConfig.java
@@ -3,11 +3,9 @@ package ch.usi.si.seart.config;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @EnableConfigurationProperties
 @ConfigurationPropertiesScan(basePackages = "ch.usi.si.seart.config.properties")
-@PropertySource("classpath:ghs.properties")
 public class PropertiesConfig {
 }
