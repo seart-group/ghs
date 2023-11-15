705d38dbb2718ba6239241b725868ed590376c19
dabico <dabico@usi.ch>
2023-11-15 10:44:42 +0100

    The `ghs.statistics.suggestion-limit` is now validated
    Annotated with `@PositiveOrZero`, it restricts the value to a natural
    number.

--- src/main/java/ch/usi/si/seart/config/properties/StatisticsProperties.java
+++ src/main/java/ch/usi/si/seart/config/properties/StatisticsProperties.java
@@ -7,11 +7,14 @@ import lombok.experimental.FieldDefaults;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
+import javax.validation.constraints.PositiveOrZero;
+
 @Getter
 @ConfigurationProperties(prefix = "ghs.statistics", ignoreUnknownFields = false)
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @AllArgsConstructor(onConstructor_ = @ConstructorBinding)
 public class StatisticsProperties {
 
+    @PositiveOrZero
     int suggestionLimit;
 }
