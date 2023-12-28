aba8a1f10f734347284c6555fdb9192269e0b1f6
dabico <dabico@usi.ch>
2023-12-28 11:56:41 +0100

    Update `GitRepoDto` to include newly introduced field

--- src/main/java/ch/usi/si/seart/dto/GitRepoDto.java
+++ src/main/java/ch/usi/si/seart/dto/GitRepoDto.java
@@ -71,6 +71,7 @@ public class GitRepoDto {
 
     Boolean hasWiki;
     Boolean isArchived;
+    Boolean isDisabled;
 
     @JacksonXmlProperty(localName = "languages", isAttribute = true)
     @Builder.Default
