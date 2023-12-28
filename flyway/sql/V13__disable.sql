b19df5d11693679bd580e39c4b6354a1d5208458
dabico <dabico@usi.ch>
2023-12-28 13:01:46 +0100

    Update migration script to include `locked` column

--- flyway/sql/V13__disable.sql
+++ flyway/sql/V13__disable.sql
@@ -2,4 +2,10 @@ ALTER TABLE git_repo
 ADD COLUMN disabled BIT NULL
 AFTER archived;
 
-UPDATE git_repo SET disabled = false;
+ALTER TABLE git_repo
+ADD COLUMN locked BIT NULL
+AFTER archived;
+
+UPDATE git_repo
+SET disabled = false
+AND locked = false;
