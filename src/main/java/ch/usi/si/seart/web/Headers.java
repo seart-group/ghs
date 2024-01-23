faed76061121fce023c53d8891eaf00370f99c60
dabico <dabico@usi.ch>
2024-01-23 10:01:25 +0100

    Create `Slf4jMDCLoggingFilter`
    This filter will allow us to associate each request with a UUID,
    allowing for easier response tracking and debugging in the log files.

--- src/main/java/ch/usi/si/seart/web/Headers.java
+++ src/main/java/ch/usi/si/seart/web/Headers.java
@@ -5,6 +5,8 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Headers {
 
+    public static final String X_REQUEST_ID = "X-Request-Id";
+
     public static final String X_LINK_SEARCH = "X-Link-Search";
 
     public static final String X_LINK_DOWNLOAD = "X-Link-Download";
---
+++ src/main/java/ch/usi/si/seart/web/filter/Slf4jMDCLoggingFilter.java
@@ -0,0 +1,33 @@
+package ch.usi.si.seart.web.filter;
+
+import ch.usi.si.seart.web.Headers;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.MDC;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+
+public class Slf4jMDCLoggingFilter extends OncePerRequestFilter {
+
+    private static final String KEY = "LoggingFilter.UUID";
+
+    @Override
+    protected void doFilterInternal(
+            @NotNull HttpServletRequest request, HttpServletResponse response, FilterChain filterChain
+    ) throws ServletException, IOException {
+        String value = UUID.randomUUID().toString();
+        MDC.put(KEY, value);
+        try {
+            response.setHeader(Headers.X_REQUEST_ID, value);
+            filterChain.doFilter(request, response);
+        } finally {
+            MDC.remove(KEY);
+        }
+    }
+}
