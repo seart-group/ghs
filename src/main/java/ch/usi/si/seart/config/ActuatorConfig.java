51a37cc78a786a1bed3f3c6517b2518e46c28a7d
dabico <dabico@usi.ch>
2024-01-23 15:02:04 +0100

    Add custom contributor for exposing Spring Core and Boot version info

--- src/main/java/ch/usi/si/seart/config/ActuatorConfig.java
+++ src/main/java/ch/usi/si/seart/config/ActuatorConfig.java
@@ -1,9 +1,15 @@
 package ch.usi.si.seart.config;
 
+import ch.usi.si.seart.actuate.info.SpringInfoContributor;
 import ch.usi.si.seart.github.GitHubAPIHealthIndicator;
+import org.springframework.boot.actuate.autoconfigure.info.ConditionalOnEnabledInfoContributor;
+import org.springframework.boot.actuate.autoconfigure.info.InfoContributorAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.info.InfoContributorFallback;
 import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
 @Configuration
 public class ActuatorConfig {
@@ -11,5 +17,12 @@ public class ActuatorConfig {
     @Bean("gitHubApi")
     public HealthIndicator gitHubApiHealthIndicator() {
         return new GitHubAPIHealthIndicator();
+    }
+
+    @Bean
+    @Order(InfoContributorAutoConfiguration.DEFAULT_ORDER)
+    @ConditionalOnEnabledInfoContributor(value = "spring", fallback = InfoContributorFallback.DISABLE)
+    public InfoContributor applicationInfoContributor() {
+        return new SpringInfoContributor();
     }
 }
