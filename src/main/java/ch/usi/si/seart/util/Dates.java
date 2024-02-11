b101b6ad0376a131b3daddb95c35d44495613ae5
dabico <dabico@usi.ch>
2024-02-11 20:57:09 +0100

    Fix: Coerce values above 9999-12-31 to null (`Dates#fromGitDateString`)

--- src/main/java/ch/usi/si/seart/util/Dates.java
+++ src/main/java/ch/usi/si/seart/util/Dates.java
@@ -2,10 +2,11 @@ package ch.usi.si.seart.util;
 
 import lombok.experimental.UtilityClass;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
-import java.util.Objects;
 import java.util.TimeZone;
 
 /**
@@ -15,6 +16,26 @@ import java.util.TimeZone;
  */
 @UtilityClass
 public class Dates {
+
+    private static final Date MYSQL_MAX;
+
+    private static final DateFormat FORMAT;
+
+    static {
+        FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        TimeZone zone = TimeZone.getTimeZone("Universal");
+        FORMAT.setTimeZone(zone);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeZone(zone);
+        calendar.set(Calendar.YEAR, 9999);
+        calendar.set(Calendar.MONTH, Calendar.DECEMBER);
+        calendar.set(Calendar.DAY_OF_MONTH, 31);
+        calendar.set(Calendar.HOUR_OF_DAY, 23);
+        calendar.set(Calendar.MINUTE, 59);
+        calendar.set(Calendar.SECOND, 59);
+        calendar.set(Calendar.MILLISECOND, 999);
+        MYSQL_MAX = calendar.getTime();
+    }
 
     /**
      * Parses a date string in the Git date
@@ -28,12 +49,10 @@ public class Dates {
      * or null if the string cannot be parsed.
      */
     public Date fromGitDateString(String date) {
-        Objects.requireNonNull(date, "Date string can not be null!");
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-        dateFormat.setTimeZone(TimeZone.getTimeZone("Universal"));
         try {
-            return dateFormat.parse(date);
-        } catch (ParseException ex) {
+            Date parsed = FORMAT.parse(date);
+            return parsed.after(MYSQL_MAX) ? MYSQL_MAX : parsed;
+        } catch (ParseException | NullPointerException ex) {
             return null;
         }
     }
