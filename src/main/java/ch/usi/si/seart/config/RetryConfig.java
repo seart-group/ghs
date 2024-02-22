6c7f9dffe80c8a47b115253ec689fc7eddc0f658
dabico <dabico@usi.ch>
2024-02-22 22:12:25 +0100

    Replace deprecated API usage in `RetryTemplate` definition

--- src/main/java/ch/usi/si/seart/config/RetryConfig.java
+++ src/main/java/ch/usi/si/seart/config/RetryConfig.java
@@ -52,7 +52,7 @@ public class RetryConfig {
     @Bean
     public RetryTemplate timeLimitedRetryTemplate(BackOffPolicy backOffPolicy) {
         return RetryTemplate.builder()
-                .withinMillis(7_200_000)
+                .withTimeout(7_200_000)
                 .customBackoff(backOffPolicy)
                 .retryOn(Exception.class)
                 .build();
