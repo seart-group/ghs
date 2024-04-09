03af403e18450381c6658e69609070d7f6c2c0a0
dabico <dabico@usi.ch>
2024-04-09 10:15:23 +0200

    Fix: indentation issues in handlebars.js

--- html/js/handlebars.js
+++ html/js/handlebars.js
@@ -76,188 +76,188 @@ Handlebars.registerHelper("between", function (item, options) {
     const min = options.hash.min || Number.MIN_SAFE_INTEGER;
     const max = options.hash.max || Number.MAX_SAFE_INTEGER;
     switch (typeof item) {
-        case "number":
-            return _.inRange(item, min, max);
-        case "string":
-            return _.inRange(item.length, min, max);
-        case "object":
-            return _.inRange(Object.keys(item).length, min, max);
-        default:
-            return false;
+    case "number":
+        return _.inRange(item, min, max);
+    case "string":
+        return _.inRange(item.length, min, max);
+    case "object":
+        return _.inRange(Object.keys(item).length, min, max);
+    default:
+        return false;
     }
 
 });
 
 Handlebars.registerHelper("devicon", function (language) {
     switch (language) {
-        // Plain mappings
-        case "Arduino":
-        case "C":
-        case "Ceylon":
-        case "Clojure":
-        case "CMake":
-        case "Dart":
-        case "Elixir":
-        case "Elm":
-        case "Erlang":
-        case "Gradle":
-        case "GraphQL":
-        case "Groovy":
-        case "Handlebars":
-        case "Haskell":
-        case "Haxe":
-        case "Java":
-        case "JavaScript":
-        case "Julia":
-        case "Kotlin":
-        case "Lua":
-        case "MATLAB":
-        case "OCaml":
-        case "PHP":
-        case "PowerShell":
-        case "Python":
-        case "Ruby":
-        case "Rust":
-        case "Sass":
-        case "Scala":
-        case "Solidity":
-        case "Svelte":
-        case "Swift":
-        case "TypeScript":
-        case "XML":
-        case "YAML":
-            return `devicon-${language.toLowerCase()}-plain`;
+    // Plain mappings
+    case "Arduino":
+    case "C":
+    case "Ceylon":
+    case "Clojure":
+    case "CMake":
+    case "Dart":
+    case "Elixir":
+    case "Elm":
+    case "Erlang":
+    case "Gradle":
+    case "GraphQL":
+    case "Groovy":
+    case "Handlebars":
+    case "Haskell":
+    case "Haxe":
+    case "Java":
+    case "JavaScript":
+    case "Julia":
+    case "Kotlin":
+    case "Lua":
+    case "MATLAB":
+    case "OCaml":
+    case "PHP":
+    case "PowerShell":
+    case "Python":
+    case "Ruby":
+    case "Rust":
+    case "Sass":
+    case "Scala":
+    case "Solidity":
+    case "Svelte":
+    case "Swift":
+    case "TypeScript":
+    case "XML":
+    case "YAML":
+        return `devicon-${language.toLowerCase()}-plain`;
         // "Original" mappings
-        case "CoffeeScript":
-        case "Markdown":
-        case "Nginx":
-        case "R":
-        case "Stylus":
-        case "Zig":
-            return `devicon-${language.toLowerCase()}-original`;
+    case "CoffeeScript":
+    case "Markdown":
+    case "Nginx":
+    case "R":
+    case "Stylus":
+    case "Zig":
+        return `devicon-${language.toLowerCase()}-original`;
         // Indirect mappings
-        case "ApacheConf":
-            return "devicon-apache-plain";
-        case "AppleScript":
-            return "devicon-apple-original";
-        case "C#":
-            return "devicon-csharp-plain";
-        case "C++":
-            return "devicon-cplusplus-plain";
-        case "CSS":
-            return "devicon-css3-plain";
-        case "Dockerfile":
-            return "devicon-docker-plain";
-        case "EmberScript":
-            return "devicon-ember-original-wordmark";
-        case "F#":
-            return "devicon-fsharp-plain";
-        case "HTML":
-            return "devicon-html5-plain";
-        case "JetBrains MPS":
-            return "devicon-jetbrains-plain";
-        case "Jupyter Notebook":
-            return "devicon-jupyter-plain";
-        case "Less":
-            return "devicon-less-plain-wordmark";
-        case "Nix":
-            return "devicon-nixos-plain";
-        case "Objective-C":
-            return "devicon-objectivec-plain";
-        case "PLSQL":
-            return "devicon-oracle-original";
-        case "PLpgSQL":
-            return "devicon-postgresql-plain";
-        case "Unity3D Asset":
-            return "devicon-unity-original";
-        case "UnrealScript":
-            return "devicon-unrealengine-original";
-        case "Vue":
-            return "devicon-vuejs-plain";
-        case "WebAssembly":
-            return "devicon-wasm-plain";
+    case "ApacheConf":
+        return "devicon-apache-plain";
+    case "AppleScript":
+        return "devicon-apple-original";
+    case "C#":
+        return "devicon-csharp-plain";
+    case "C++":
+        return "devicon-cplusplus-plain";
+    case "CSS":
+        return "devicon-css3-plain";
+    case "Dockerfile":
+        return "devicon-docker-plain";
+    case "EmberScript":
+        return "devicon-ember-original-wordmark";
+    case "F#":
+        return "devicon-fsharp-plain";
+    case "HTML":
+        return "devicon-html5-plain";
+    case "JetBrains MPS":
+        return "devicon-jetbrains-plain";
+    case "Jupyter Notebook":
+        return "devicon-jupyter-plain";
+    case "Less":
+        return "devicon-less-plain-wordmark";
+    case "Nix":
+        return "devicon-nixos-plain";
+    case "Objective-C":
+        return "devicon-objectivec-plain";
+    case "PLSQL":
+        return "devicon-oracle-original";
+    case "PLpgSQL":
+        return "devicon-postgresql-plain";
+    case "Unity3D Asset":
+        return "devicon-unity-original";
+    case "UnrealScript":
+        return "devicon-unrealengine-original";
+    case "Vue":
+        return "devicon-vuejs-plain";
+    case "WebAssembly":
+        return "devicon-wasm-plain";
         // Multi-mappings
-        case "Go":
-        case "Go Checksums":
-        case "Go Module":
-            return "devicon-go-plain";
-        case "LLVM":
-        case "LLVM IR":
-            return "devicon-llvm-plain";
-        case "Perl":
-        case "Perl 6":
-            return "devicon-perl-plain";
-        case "QMake":
-        case "Qt Script":
-            return "devicon-qt-original";
-        case "Shell":
-        case "ShellSession":
-            return "devicon-bash-plain";
-        case "PlantUML":
-            return "devicon-unifiedmodelinglanguage-plain";
-        case "Vim Script":
-        case "Vim Snippet":
-            return "devicon-vim-plain";
-        case "Visual Basic":
-        case "Visual Basic .NET":
-        case "Visual Basic 6.0":
-            return "devicon-visualbasic-plain";
-        case "Microsoft Visual Studio Solution":
-            return "devicon-visualstudio-plain";
+    case "Go":
+    case "Go Checksums":
+    case "Go Module":
+        return "devicon-go-plain";
+    case "LLVM":
+    case "LLVM IR":
+        return "devicon-llvm-plain";
+    case "Perl":
+    case "Perl 6":
+        return "devicon-perl-plain";
+    case "QMake":
+    case "Qt Script":
+        return "devicon-qt-original";
+    case "Shell":
+    case "ShellSession":
+        return "devicon-bash-plain";
+    case "PlantUML":
+        return "devicon-unifiedmodelinglanguage-plain";
+    case "Vim Script":
+    case "Vim Snippet":
+        return "devicon-vim-plain";
+    case "Visual Basic":
+    case "Visual Basic .NET":
+    case "Visual Basic 6.0":
+        return "devicon-visualbasic-plain";
+    case "Microsoft Visual Studio Solution":
+        return "devicon-visualstudio-plain";
         // No mapping
-        default:
-            return "";
+    default:
+        return "";
     }
 });
 
 Handlebars.registerHelper("octicon", function (key) {
     switch (key) {
-        case "commits":
-            return "commit";
-        case "watchers":
-            return "eye";
-        case "stars":
-            return "star";
-        case "forks":
-            return "repo-forked";
-        case "branches":
-            return "git-branch";
-        case "contributors":
-            return "people";
-        case "totalIssues":
-        case "openIssues":
-            return "issue-opened";
-        case "totalPullReqs":
-        case "openPullReqs":
-            return "git-pull-request";
-        case "releases":
-            return "tag";
-        case "size":
-            return "file-code";
-        case "created":
-            return "plus";
-        case "updated":
-            return "pencil";
-        case "lastPush":
-            return "arrow-up";
-        case "lastCommit":
-            return "file-diff";
-        case "codeLines":
-            return "code";
-        case "commentLines":
-            return "comment";
-        default:
-            return "question";
+    case "commits":
+        return "commit";
+    case "watchers":
+        return "eye";
+    case "stars":
+        return "star";
+    case "forks":
+        return "repo-forked";
+    case "branches":
+        return "git-branch";
+    case "contributors":
+        return "people";
+    case "totalIssues":
+    case "openIssues":
+        return "issue-opened";
+    case "totalPullReqs":
+    case "openPullReqs":
+        return "git-pull-request";
+    case "releases":
+        return "tag";
+    case "size":
+        return "file-code";
+    case "created":
+        return "plus";
+    case "updated":
+        return "pencil";
+    case "lastPush":
+        return "arrow-up";
+    case "lastCommit":
+        return "file-diff";
+    case "codeLines":
+        return "code";
+    case "commentLines":
+        return "comment";
+    default:
+        return "question";
     }
 });
 
 Handlebars.registerHelper("other-styles", function (key) {
-   switch (key) {
-       case "openIssues":
-       case "openPullReqs":
-           return "text-danger";
-       case "blankLines":
-           return "opacity-0";
-       default: return "";
-   }
+    switch (key) {
+    case "openIssues":
+    case "openPullReqs":
+        return "text-danger";
+    case "blankLines":
+        return "opacity-0";
+    default: return "";
+    }
 });
