6dec1e7821f50166ed4dbeda4980f576303c4080
dabico <dabico@usi.ch>
2024-04-10 10:35:58 +0200

    Use new autocomplete for `language` and `license` fields
    First in a series of commits centered on replacing the `typeahead`
    library with the `autocomplete`. The latter provides more
    functionalities while being visually equivalent to the former.

---
+++ html/js/language.js
@@ -0,0 +1,52 @@
+import Autocomplete from "https://cdn.jsdelivr.net/npm/bootstrap5-autocomplete@1.1.25/autocomplete.min.js";
+
+(function (base, $, Autocomplete) {
+    const $toast_container = $(".toast-container");
+    const $search_language = $("#search-language");
+    const $search_license = $("#search-license");
+    const $search_language_list = $("#search-language-list");
+    const $search_license_list = $("#search-license-list");
+    const config = {
+        notFoundMessage: "No suggestions available...",
+        highlightClass: "bg-transparent text-current p-0",
+        activeClasses: [ "bg-secondary", "text-white" ],
+        suggestionsThreshold: 0,
+        maximumItems: 10,
+        debounceTime: 250,
+        autoselectFirst: false,
+        highlightTyped: true,
+        fullWidth: true,
+    };
+
+    const create_option = (value) => `<option value="${value}">${value}</option>`;
+
+    fetch(`${base}/l`)
+        .then(response => response.json())
+        .then(data => {
+            const html = data.map(create_option).join("");
+            $search_language_list.append(html);
+        })
+        .then(() => {
+            const [ element ] = $search_language.get();
+            new Autocomplete(element, config);
+        })
+        .catch(() => $toast_container.twbsToast(({
+            id: "language-toast",
+            body: "Could not retrieve language suggestions!",
+        })));
+
+    fetch(`${base}/r/licenses`)
+        .then(response => response.json())
+        .then(data => {
+            const html = data.map(create_option).join("");
+            $search_license_list.append(html);
+        })
+        .then(() => {
+            const [ element ] = $search_license.get();
+            new Autocomplete(element, config);
+        })
+        .catch(() => $toast_container.twbsToast(({
+            id: "license-toast",
+            body: "Could not retrieve license suggestions!",
+        })));
+})(base, jQuery, Autocomplete);
--- html/js/typeahead.js
+++ html/js/typeahead.js
@@ -10,9 +10,7 @@
 
     const $toast_container = $(".toast-container");
     const $search_label = $("#search-label");
-    const $search_license = $("#search-license");
     const $search_topic = $("#search-topic");
-    const $search_language = $("#search-language");
 
     const options = (name) => ({ id: `${name}-toast`, body: `Could not retrieve ${name} suggestions!` });
 
@@ -21,18 +19,8 @@
         .then(data => $search_label.typeahead({ ...typeaheadOptions, source: data }))
         .catch(() => $toast_container.twbsToast(options("label")));
 
-    fetch(`${base}/r/licenses`)
-        .then(response => response.json())
-        .then(data => $search_license.typeahead({ ...typeaheadOptions, source: data }))
-        .catch(() => $toast_container.twbsToast(options("license")));
-
     fetch(`${base}/r/topics`)
         .then(response => response.json())
         .then(data => $search_topic.typeahead({ ...typeaheadOptions, source: data }))
         .catch(() => $toast_container.twbsToast(options("topic")));
-
-    fetch(`${base}/l`)
-        .then(response => response.json())
-        .then(data => $search_language.typeahead({ ...typeaheadOptions, source: data }))
-        .catch(() => $toast_container.twbsToast(options("language")));
 }(base, jQuery));
