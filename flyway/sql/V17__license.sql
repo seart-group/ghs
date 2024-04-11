94acbc10fdfdf7c1338e47eae62ed64754a1af36
dabico <dabico@usi.ch>
2024-04-11 12:30:23 +0200

    Materialize `license_statistics` on server startup
    The view will not be enough unfortunately, the join performance seems
    to be quite poor without a foreign key. For this reason I changed
    `license_statistics` from a view to a regular table, akin to the `label`
    and `topic` counterparts.

--- flyway/sql/V17__license.sql
+++ flyway/sql/V17__license.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE VIEW license_statistics AS
+CREATE OR REPLACE VIEW count_git_repo_by_license AS
 SELECT
     license_id AS license_id,
     COUNT(id) AS count
@@ -6,3 +6,12 @@ FROM git_repo
 WHERE license_id IS NOT NULL
 GROUP BY license_id
 ORDER BY count DESC;
+
+CREATE TABLE license_statistics (
+    license_id BIGINT NOT NULL
+        PRIMARY KEY,
+    count BIGINT NOT NULL
+)
+SELECT * FROM count_git_repo_by_license;
+
+CREATE INDEX count_idx ON license_statistics (count);
