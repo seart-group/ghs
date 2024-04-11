94d91db6143687b2f0f9a074827c7d19f75ddc5e
dabico <dabico@usi.ch>
2024-04-11 11:05:57 +0200

    Map new `license_statistics` view to `License.Statistics` inner class

--- src/main/java/ch/usi/si/seart/model/License.java
+++ src/main/java/ch/usi/si/seart/model/License.java
@@ -7,12 +7,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.Immutable;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import java.util.HashSet;
@@ -40,4 +46,39 @@ public class License {
     @Builder.Default
     @OneToMany(mappedBy = "license")
     Set<GitRepo> repos = new HashSet<>();
+
+    @PrimaryKeyJoinColumn
+    @OneToOne(
+            mappedBy = "license",
+            cascade = {
+                CascadeType.PERSIST,
+                CascadeType.MERGE,
+                CascadeType.REFRESH,
+                CascadeType.DETACH
+            }
+    )
+    Statistics statistics;
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Entity
+    @Immutable
+    @Table(name = "license_statistics")
+    public static class Statistics {
+
+        @Id
+        @Column(name = "license_id")
+        Long id;
+
+        @OneToOne(optional = false)
+        @MapsId("id")
+        @JoinColumn(name = "license_id")
+        License license;
+
+        @Builder.Default
+        @Column(name = "count")
+        Long count = 0L;
+    }
 }
