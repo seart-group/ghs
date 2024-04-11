3a9c5745c740b9f11a2124a1c8ca512073e114cf
dabico <dabico@usi.ch>
2024-04-11 17:08:47 +0200

    Create hourly event for refreshing license statistics

--- flyway/sql/R__materialize.sql
+++
@@ -1,7 +0,0 @@
--- ${flyway:timestamp}
-
-UPDATE license_statistics
-INNER JOIN count_git_repo_by_license
-    ON license_statistics.license_id = count_git_repo_by_license.license_id
-SET license_statistics.count = count_git_repo_by_license.count
-WHERE license_statistics.count != count_git_repo_by_license.count;
