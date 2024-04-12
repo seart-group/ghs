b214b7b0db63492947edaecae9652b8f4adced71
dabico <dabico@usi.ch>
2024-04-12 19:51:22 +0200

    Replace `typeahead` with the superior `autocomplete`

--- html/js/autocomplete.js
+++ html/js/autocomplete.js
@@ -1,52 +1,43 @@
 import Autocomplete from "https://cdn.jsdelivr.net/npm/bootstrap5-autocomplete@1.1.25/autocomplete.min.js";
 
 (function (base, $, Autocomplete) {
-    const $toast_container = $(".toast-container");
-    const $search_language = $("#search-language");
-    const $search_license = $("#search-license");
-    const $search_language_list = $("#search-language-list");
-    const $search_license_list = $("#search-license-list");
-    const config = {
+    const default_config = {
         notFoundMessage: "No suggestions available...",
         highlightClass: "bg-transparent text-current text-decoration-underline p-0",
         activeClasses: [ "bg-secondary", "text-white" ],
         suggestionsThreshold: 0,
         maximumItems: 10,
-        debounceTime: 250,
         autoselectFirst: false,
         highlightTyped: true,
         fullWidth: true,
     };
 
-    const create_option = (value) => `<option value="${value}">${value}</option>`;
+    const get_response_items = response => response.json().then(({ items }) => items);
+    const toggle_spinner = instance => {
+        const input = instance.getInput();
+        $(`#${input.id}-spinner`).toggleClass("d-none");
+    };
 
-    fetch(`${base}/l`)
-        .then(response => response.json())
-        .then(data => {
-            const html = data.map(create_option).join("");
-            $search_language_list.append(html);
-        })
-        .then(() => {
-            const [ element ] = $search_language.get();
-            new Autocomplete(element, config);
-        })
-        .catch(() => $toast_container.twbsToast(({
-            id: "language-toast",
-            body: "Could not retrieve language suggestions!",
-        })));
+    const server_config = {
+        liveServer: true,
+        noCache: false,
+        debounceTime: 250,
+        queryParam: "name",
+        serverParams: {
+            page: 0,
+            size: 10,
+        },
+        onServerResponse: get_response_items,
+        onBeforeFetch: toggle_spinner,
+        onAfterFetch: toggle_spinner,
+    };
 
-    fetch(`${base}/r/licenses?${ new URLSearchParams({ size: 100 }) }`)
-        .then(response => response.json())
-        .then(data => {
-            const html = data.map(create_option).join("");
-            $search_license_list.append(html);
-        })
-        .then(() => {
-            const [ element ] = $search_license.get();
-            new Autocomplete(element, config);
-        })
-        .catch(() => $toast_container.twbsToast(({
-            id: "license-toast",
-            body: "Could not retrieve license suggestions!",
-        })));
+    $(".autocomplete").each((index, element) => {
+        const endpoint = element.dataset.endpoint;
+        new Autocomplete(element, {
+            server: base + endpoint,
+            ...default_config,
+            ...server_config
+        });
+    });
 })(base, jQuery, Autocomplete);
