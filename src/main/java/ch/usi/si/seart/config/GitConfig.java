18f81f455bfd86cd42f83a046da8e4dec87279ec
dabico <dabico@usi.ch>
2024-07-08 11:10:40 +0200

    Add optional `CredentialsProvider` bean definition
    Will only be created if the necessary username and password properties
    are specified.

--- src/main/java/ch/usi/si/seart/config/GitConfig.java
+++ src/main/java/ch/usi/si/seart/config/GitConfig.java
@@ -1,7 +1,21 @@
 package ch.usi.si.seart.config;
 
+import ch.usi.si.seart.config.properties.GitProperties;
+import org.eclipse.jgit.transport.CredentialsProvider;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GitConfig {
+
+    @Bean
+    @ConditionalOnExpression("not '${ghs.git.username}'.blank and not '${ghs.git.password}'.blank")
+    CredentialsProvider credentialsProvider(GitProperties gitProperties) {
+        return new UsernamePasswordCredentialsProvider(
+                gitProperties.getUsername(),
+                gitProperties.getPassword()
+        );
+    }
 }
