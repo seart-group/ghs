1cbb795a146813d6439f77ec7687e373284e11bb
dabico <dabico@usi.ch>
2024-07-17 09:18:12 +0200

    Add `InitializingBean` for setting the JSON mapper used by `cloc`

--- src/main/java/ch/usi/si/seart/config/CLOCConfig.java
+++ src/main/java/ch/usi/si/seart/config/CLOCConfig.java
@@ -1,7 +1,16 @@
 package ch.usi.si.seart.config;
 
+import ch.usi.si.seart.cloc.CLOCCommand;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CLOCConfig {
+
+    @Bean
+    InitializingBean clocInitializingBean(JsonMapper jsonMapper) {
+        return () -> CLOCCommand.setOutputMapper(jsonMapper);
+    }
 }
