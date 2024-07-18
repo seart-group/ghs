3d7a860982e8654e078f2ad46b2f244d059ca527
dabico <dabico@usi.ch>
2024-07-18 13:55:18 +0200

    Add `CLOCCommand.Builder` prototype-scoped beans
    Autowiring or requesting instances will yield builders with already
    initialized settings. Currently only supports the timeout settings.

--- src/main/java/ch/usi/si/seart/config/CLOCConfig.java
+++ src/main/java/ch/usi/si/seart/config/CLOCConfig.java
@@ -1,10 +1,15 @@
 package ch.usi.si.seart.config;
 
 import ch.usi.si.seart.cloc.CLOCCommand;
+import ch.usi.si.seart.config.properties.CLOCProperties;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import java.time.Duration;
 
 @Configuration
 public class CLOCConfig {
@@ -12,5 +17,13 @@ public class CLOCConfig {
     @Bean
     InitializingBean clocInitializingBean(JsonMapper jsonMapper) {
         return () -> CLOCCommand.setOutputMapper(jsonMapper);
+    }
+
+    @Bean
+    @Scope(scopeName = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public CLOCCommand.Builder clocCommandBuilder(CLOCProperties properties) {
+        Duration duration = properties.getAnalysisTimeoutDuration();
+        int seconds = Math.toIntExact(duration.getSeconds());
+        return CLOCCommand.create().withTimeout(seconds);
     }
 }
